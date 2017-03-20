bdc399c1850822a7a5402461224a159bd7957ddc
Gábor Domonkos <gabor.domonkos@lombiq.com>
2017-03-20 15:34:06 +0100

    Adding YourFirstOrchardController
    --HG--
    branch : issue/ORCH2-1

---
+++ Controllers/YourFirstOrchardController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OrchardHUN.TrainingDemo.Controllers
+{
+    public class YourFirstOrchardController : Controller
+    {
+        public ActionResult Index()
+        {
+            // For now we just return an empty view. This action is accessible from under
+            // OrchardHUN.TrainingDemo/YourFirstOrchard route (appended to your site's root path; so using defaults it
+            // would look something like this:
+            // http://localhost:2918/OrchardHUN.TrainingDemo/YourFirstOrchard/Index) If you don't know how this
+            // path gets together take a second look at how ASP.NET MVC routing works!
+
+            return View();
+
+            // NEXT STEP: take a look at the view we're using at Views/YourFirstOrchard/Index
+        }
+    }
+}
