05939d8e66824d657b0aa46670e0517eecc7caf3
Gábor Domonkos <gabor.domonkos@lombiq.com>
2017-03-31 21:15:06 +0200

    Adding ContentTypeNames static class
    --HG--
    branch : issue/ORCH2-1

---
+++ Constants/ContentTypeNames.cs
@@ -0,0 +1,11 @@
+﻿namespace OrchardHUN.TrainingDemo.Constants
+{
+    public static class ContentTypeNames
+    {
+        public const string Person = nameof(Person);
+
+        public const string Address = nameof(Address);
+
+        public const string PersonWithAddresses = nameof(PersonWithAddresses);
+    }
+}
