791c0d84e4ebf48fa01168cce9867d1b651c09ff
Gábor Domonkos <gabor.domonkos@lombiq.com>
2017-03-31 19:44:08 +0200

    Refactoring Migrations to PersonMigration.cs and adding AddressMigration.cs
    --HG--
    branch : issue/ORCH2-1

---
+++ Migrations/AddressMigration.cs
@@ -0,0 +1,35 @@
+﻿using Orchard.ContentManagement.Metadata.Settings;
+using Orchard.ContentManagement.MetaData;
+using Orchard.Data.Migration;
+using OrchardHUN.TrainingDemo.Models;
+
+namespace OrchardHUN.TrainingDemo.Migrations
+{
+    public class AddressMigration : DataMigration
+    {
+        IContentDefinitionManager _contentDefinitionManager;
+
+
+        public AddressMigration(IContentDefinitionManager contentDefinitionManager)
+        {
+            _contentDefinitionManager = contentDefinitionManager;
+        }
+
+
+        public int Create()
+        {
+            _contentDefinitionManager.AlterPartDefinition(nameof(AddressPart), builder => builder
+                .Reusable()
+                .Attachable()
+                .WithDescription("Provides city, ZIP Code and address fields to your content item.")
+            );
+
+            _contentDefinitionManager.AlterTypeDefinition(nameof(Constants.ContentTypeNames.Address), builder => builder
+                .Creatable()
+                .WithPart(nameof(AddressPart))
+            );
+
+            return 1;
+        }
+    }
+}
---
+++ Migrations/PersonMigration.cs
@@ -0,0 +1,65 @@
+﻿using Orchard.ContentFields.Fields;
+using Orchard.ContentManagement.Metadata.Settings;
+using Orchard.ContentManagement.MetaData;
+using Orchard.Data.Migration;
+using OrchardHUN.TrainingDemo.Constants;
+using OrchardHUN.TrainingDemo.Models;
+using static OrchardHUN.TrainingDemo.Constants.ContentFieldNames;
+
+namespace OrchardHUN.TrainingDemo.Migrations
+{
+    public class PersonMigration : DataMigration
+    {
+        IContentDefinitionManager _contentDefinitionManager;
+
+
+        public PersonMigration(IContentDefinitionManager contentDefinitionManager)
+        {
+            _contentDefinitionManager = contentDefinitionManager;
+        }
+
+
+        public int Create()
+        {
+            _contentDefinitionManager.AlterPartDefinition(nameof(PersonPart), builder => builder
+                .Attachable()
+                .WithField(nameof(Name), cfg => cfg
+                    .OfType(nameof(TextField))
+                    .WithDisplayName("The name of the person."))
+                .WithField(nameof(ContentFieldNames.Sex), cfg => cfg
+                    .OfType(nameof(TextField))
+                    .WithDisplayName("The sex of the person."))
+                .WithField(nameof(ProfessionalProfile), cfg => cfg
+                    .OfType(nameof(LinkField))
+                    .WithDisplayName("The professional profile URL of the person.")
+                    .WithSetting("LinkFieldSettings.DefaultUrl", "https://lombiq.com")
+                    .WithSetting("LinkFieldSettings.DefaultText", "Lombiq.com"))
+                .WithField(nameof(Biograpy), cfg => cfg
+                    .OfType(nameof(TextField))
+                    .WithDisplayName("The short biography of the person."))
+            );
+
+            // Creating a Person content type without the reusable AddressParts.
+            _contentDefinitionManager.AlterTypeDefinition(nameof(ContentTypeNames.Person), builder => builder
+                .Creatable()
+                .WithPart(nameof(PersonPart))
+            );
+
+            // Creating the PersonWithAddresses content type with the reusable AddressParts.
+            _contentDefinitionManager.AlterTypeDefinition(nameof(PersonWithAddresses), builder => builder
+                .Creatable()
+                .WithPart(nameof(PersonPart))
+                .WithPart("HomeAddress", nameof(AddressPart), cfg => cfg
+                    .WithDisplayName("The home address of the person."))
+                .WithPart("WorkAddress", nameof(AddressPart), cfg => cfg
+                    .WithDisplayName("The work address of the person."))
+            );
+
+            SchemaBuilder.CreateMapIndexTable(nameof(PersonPartIndex), table => table
+                .Column<string>(nameof(PersonPartIndex.Name), col => col.WithLength(1024))
+            );
+
+            return 1;
+        }
+    }
+}
