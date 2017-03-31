c4061cf8ea73a4ede80fa840b16a72fa963bd718
Gábor Domonkos <gabor.domonkos@lombiq.com>
2017-03-31 21:14:42 +0200

    Adding PersonWithAddresses skeleton
    --HG--
    branch : issue/ORCH2-1

---
+++ Models/PersonWithAddresses.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OrchardHUN.TrainingDemo.Models
+{
+    public class PersonWithAddresses
+    {
+        public PersonPart PersonPart { get; set; }
+
+        public AddressPart WorkAddress { get; set; }
+
+        public AddressPart HomeAddress { get; set; }
+
+    }
+}
