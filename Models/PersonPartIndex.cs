8ef38046e05e48ee0cdefbf9b442af4808c7babb
Gábor Domonkos <gabor.domonkos@lombiq.com>
2017-03-31 19:47:48 +0200

    Adding PersonPartIndex.cs to be able to query on PersonParts with YesSql
    --HG--
    branch : issue/ORCH2-1

---
+++ Models/PersonPartIndex.cs
@@ -0,0 +1,34 @@
+﻿using YesSql.Core.Indexes;
+
+namespace OrchardHUN.TrainingDemo.Models
+{
+    public class PersonPartIndex : MapIndex
+    {
+        public string ContentItemId { get; set; }
+
+        public string Name { get; set; }
+    }
+
+    public class PersonPartIndexProvider : IndexProvider<PersonPart>
+    {
+        public override void Describe(DescribeContext<PersonPart> context)
+        {
+            context.For<PersonPartIndex>()
+                .Map(person =>
+                {
+                    var personName = person?.Name.Text;
+                    var contentItemId = person?.ContentItem.ContentItemId;
+                    if (!string.IsNullOrEmpty(personName))
+                    {
+                        return new PersonPartIndex
+                        {
+                            ContentItemId = contentItemId,
+                            Name = personName
+                        };
+                    }
+
+                    return null;
+                });
+        }
+    }
+}
