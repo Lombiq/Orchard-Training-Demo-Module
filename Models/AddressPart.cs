b55a75583c7cc040300a280df677d77420263a4d
Gábor Domonkos <gabor.domonkos@lombiq.com>
2017-03-31 19:56:33 +0200

    Adding AddressPart
    --HG--
    branch : issue/ORCH2-1

---
+++ Models/AddressPart.cs
@@ -0,0 +1,18 @@
+﻿using Orchard.ContentManagement;
+
+namespace OrchardHUN.TrainingDemo.Models
+{
+    public class AddressPart : ContentPart
+    {
+        public string City { get; set; }
+        public string ZipCode { get; set; }
+        public string Address { get; set; }
+    }
+
+
+    public enum AddressType
+    {
+        WorkAddress,
+        HomeAddress
+    }
+}
