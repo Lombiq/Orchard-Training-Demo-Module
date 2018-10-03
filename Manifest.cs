cefcec47dbda5bce988c5e028038bd4d3ef75d81
Márk Bartha <barthamark@gmail.com>
2018-10-03 17:21:52 +0200

    Adding documentation for Manifest.cs
    --HG--
    branch : issue/OCORE-1

--- Manifest.cs
+++ Manifest.cs
@@ -1,10 +1,18 @@
 using OrchardCore.Modules.Manifest;
 
 [assembly: Module(
-    Name = "Lombiq.TrainingDemo",
-    Author = "The Orchard Team",
+    // Name of the module to be displayed on the Modules page of the Dashboard.
+    Name = "Orchard Core Training Demo",
+    // Your name, company or any name that identifies the developers working on the project.
+    Author = "Lombiq",
+    // Optionally you can add a website URL (e.g. your company's website, GitHub reporitory URL).
     Website = "http://orchardproject.net",
-    Version = "0.0.1",
-    Description = "Lombiq.TrainingDemo",
-    Category = "Content Management"
+    // Version of the module.
+    Version = "2.0",
+    // Short description of the module. It will be displayed on the the Dashboard.
+    Description = "Orchard Core training demo module for teaching Orchard Core fundamentals primarily by going through its source code.",
+    // Modules are categorized on the Dashboard so it's a good idea to put similar modules together to a separate section.
+    Category = "Training"
 )]
+
+// If you're done reading throught this file go to Controllers/YourFirstOrchardCoreController.
