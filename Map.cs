37bca083c2c96b590a03cabb3573ae9b7f043a14
Márk Bartha <barthamark@gmail.com>
2018-12-01 11:36:44 +0100

    Adding Map.cs
    --HG--
    branch : issue/OCORE-1

---
+++ Map.cs
@@ -0,0 +1,98 @@
+using Lombiq.TrainingDemo.Controllers;
+using Lombiq.TrainingDemo.Drivers;
+using Lombiq.TrainingDemo.Fields;
+using Lombiq.TrainingDemo.Indexes;
+using Lombiq.TrainingDemo.Indexing;
+using Lombiq.TrainingDemo.Models;
+using Lombiq.TrainingDemo.Settings;
+using Lombiq.TrainingDemo.ViewModels;
+
+/* 
+ * In this file, you'll find the index of the whole (or at least most of the) module's classes for easier navigation 
+ * between topics. You can navigate directly to classes and their methods by clicking on their names (enclosed in a 
+ * Factory<T>() ) and pressing F12.
+ * 
+ * This class is not doing anything and only serves as an easy to use table of contents.
+ */
+namespace Lombiq.TrainingDemo
+{
+    static class Map
+    {
+        private static T Factory<T>() => default(T);
+
+        private static void Treasure()
+        {
+            // Manifest.cs: module manifest and dependencies
+
+            // Static resources: styles and scripts
+            // Declaration
+            Factory<ResourceManifest>();
+
+            // Usage: require/include
+            // Views/PersonListDashboard
+
+
+            // Basic controller demonstrating localization, Notifier, routing
+            Factory<YourFirstOrchardCoreController>();
+
+
+            // Display management, IDisplayManager
+            Factory<DisplayManagementController>();
+
+            // Display types, zones, placement
+            // Views/Book.cshtml
+            // Views/Book.Description.cshtml
+            Factory<BookDisplayDriver>();
+
+
+            // ContentPart, ContentField on ContentPart
+            Factory<PersonPart>();
+
+            // Displaying, editing and updating ContentPart
+            // Views/PersonPart.cshtml
+            // Views/PersonPart.Edit.cshtml
+            // Views/PersonPart.Summary.cshtml built-in Summary display type
+            // Views/PersonPart.SummaryAdmin.cshtml built-in SummaryAdmin display type
+            Factory<PersonPartDisplayDriver>();
+
+            // Validating ContentPart fields
+            Factory<PersonPartViewModel>();
+
+            // IndexProvider, indexing ContentPart in records
+            Factory<PersonPartIndex>();
+
+            // Content Type, ContentPart, ContentField, index record creation.
+            Factory<Migrations>();
+
+            // ISession, IContentItemDisplayManager, IClock
+            Factory<PersonListController>();
+
+
+            // ContentField
+            Factory<ColorField>();
+
+            // ContentFieldSettings
+            Factory<ColorFieldSettings>();
+
+            // Editing and updating ContentFieldSettings
+            // Views/ColorFieldSettings.Edit.cshtml
+            Factory<ColorFieldSettingsDriver>();
+
+            // Displaying, editing and updating ContentField
+            // Views/ColorField.cshtml
+            // Views/ColorField.Edit.cshtml
+            // Views/ColorField.Option.cshtml default editor option name
+            // Views/ColorField-ColorPicker.Option.cshtml custom editor option name
+            // Views/ColorField-ColorPicker.Edit.cshtml custom editor option editor
+            Factory<ColorFieldDisplayDriver>();
+
+            // ContentFieldIndexHandler, indexing ContentField using custom index provider (e.g. Lucene).
+            Factory<ColorFieldIndexHandler>();
+
+
+            // ResourceManifest, scripts, styles
+            // Views/ColorField-ColorPicker.Edit.cshtml resource injection
+            Factory<ResourceManifest>();
+        }
+    }
+}
