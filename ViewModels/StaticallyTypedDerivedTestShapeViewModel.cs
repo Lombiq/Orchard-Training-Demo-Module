eb042e2c6b89ebf9b9c42897f304969f653bdf19
Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>
2021-08-16 16:19:22 +0200

    Adding a different statically defined shape sample

---
+++ ViewModels/StaticallyTypedDerivedTestShapeViewModel.cs
@@ -0,0 +1,12 @@
+using OrchardCore.DisplayManagement.Views;
+
+namespace Lombiq.TrainingDemo.ViewModels
+{
+    public class StaticallyTypedDerivedTestShapeViewModel : ShapeViewModel
+    {
+        public int SomeValue { get; set; }
+        public int SomeOtherValue { get; set; }
+
+        public StaticallyTypedDerivedTestShapeViewModel() => Metadata.Type = "StaticallyTypedDerivedTestShape";
+    }
+}
