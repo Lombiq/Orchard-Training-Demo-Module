2e0a7fd165f4f66fd3ec526e159879828c475883
Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>
2024-02-22 16:23:28 +0100

    Clean-up

--- Lombiq.TrainingDemo.Web/Program.cs
+++ Lombiq.TrainingDemo.Web/Program.cs
@@ -4,21 +4,15 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Host.UseNLogHost();
 
-builder.Services
-    .AddOrchardCms()
-// // Orchard Specific Pipeline
-// .ConfigureServices( services => {
-// })
-// .Configure( (app, routes, services) => {
-// })
-;
+builder.Services.AddOrchardCms();
 
 var app = builder.Build();
 
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see
+    // https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
