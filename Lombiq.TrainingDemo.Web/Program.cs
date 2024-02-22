7b14eb8499ba145eb8ac2ac808f68c4de8ae0d3a
Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>
2024-02-22 16:55:38 +0100

    Removing unneeded production app setup

--- Lombiq.TrainingDemo.Web/Program.cs
+++ Lombiq.TrainingDemo.Web/Program.cs
@@ -8,17 +8,8 @@ builder.Services.AddOrchardCms();
 
 var app = builder.Build();
 
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see
-    // https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
-
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-
-app.UseOrchardCore();
+app
+    .UseStaticFiles()
+    .UseOrchardCore();
 
 app.Run();
